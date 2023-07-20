3fcf57966c8b32b72b6ef9c01c139a97c78d1eb4
enrico <enrico.rossini.uk@live.com>
2023-07-20 19:25:14 +0100

    Push example

---
+++ PickerError/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PickerError;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ PickerError/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PickerError
+{
+	public partial class MainPageViewModel : ObservableObject
+
+	{
+		public MainPageViewModel()
+		{
+			LanguageNative = "Spanish";
+		}
+
+		public List<string> SupportedLanguages { get; } = new List<string>() { "English", "French", "German", "Italian", "Spanish" };
+
+        [ObservableProperty] private string _languageNative;
+    }
+}
+
